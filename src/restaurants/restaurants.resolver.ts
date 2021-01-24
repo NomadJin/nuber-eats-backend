cf0cd528ca71f2cecf45eae7fa396891bb70cc45
Jinnyeon <jinnykim000@gmail.com>
2021-01-24 23:18:16 +0900

    start user module

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { RestaurantsService } from './restaurants.service';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 
 @Resolver(() => Restaurant)
 export class RestaurantsResolver {
@@ -14,10 +15,24 @@ export class RestaurantsResolver {
 
   @Mutation(() => Boolean)
   async createRestaurants(
-    @Args('input') createRestaurantInput: CreateRestaurantDto,
+    @Args('input') CreateRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    console.log(CreateRestaurantDto);
+    try {
+      await this.restaurantsService.createRestaurant(CreateRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
   ): Promise<boolean> {
     try {
-      await this.restaurantsService.createRestaurant(createRestaurantInput);
+      await this.restaurantsService.updateRestaurant(updateRestaurantDto);
       return true;
     } catch (e) {
       console.log(e);
